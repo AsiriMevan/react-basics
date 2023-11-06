cf38debe8d2c23492ce083aae74fe0ce45bc4860
Mevan Senanayake <mevan.senanayake@digiratina.com>
2023-11-06 16:23:38 +0530

    first commit

---
+++ src/Components/Home.js
@@ -0,0 +1,22 @@
+import useFetch from "../Hooks/useFetch";
+import BlogList from "./BlogList";
+
+const Home = () => {
+
+  const {data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs');
+
+  // const handleDelete = (id) => {
+  //   const newBlogs = blogs.filter((blog) => blog.id !== id);
+  //   setBlogs(newBlogs);
+  // };
+
+  return (
+    <div className="home">
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
+    </div>
+  );
+};
+
+export default Home;
